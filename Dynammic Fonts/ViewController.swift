125184a262f6f1cfd823b4d400fae2a8baeb4685
Samyak Pawar <samyak.professional@gmail.com>
2022-03-25 20:56:34 +0530

    Initial Commit

---
+++ Dynammic Fonts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dynammic Fonts
+//
+//  Created by Samyak Pawar on 25/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
