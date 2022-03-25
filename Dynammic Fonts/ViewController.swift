ceeed1c0d4e9849734f28d3574cc7a80daf5c123
Samyak Pawar <samyak.professional@gmail.com>
2022-03-25 21:02:35 +0530

    project UI setup complete.

--- Dynammic Fonts/ViewController.swift
+++ Dynammic Fonts/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
