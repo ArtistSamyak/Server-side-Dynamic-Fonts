85088ba6c122f6a88b97fd4df4375e39e1b9f4da
Samyak Pawar <samyak.professional@gmail.com>
2022-03-25 21:51:45 +0530

    basic implementation complete.

--- Dynammic Fonts/ViewController.swift
+++ Dynammic Fonts/ViewController.swift
@@ -14,8 +14,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let fontURL = URL(string: "http://fonts.gstatic.com/s/alumnisans/v9/nwpHtKqkOwdO2aOIwhWudEWpx_zq_Xna-Xd95uhQqFsJ3C8qng.ttf")
+        FileDownloader.loadFileAsync(url: fontURL!) { (path, error) in
+            print("Font .ttf File downloaded to : \(path!)")
+            let font = self.getFont(path: path!)
+            self.displayLabel.font = font
+        }
     }
+    
+    private func getFont(path: String) -> UIFont? {
+        
+        guard let fontFile = NSData(contentsOfFile: path)
+        else {
+            print("Font file not found?")
+            return nil
+        }
 
+        guard let provider = CGDataProvider(data: fontFile)
+        else {
+            print("Failed to create DataProvider")
+            return nil
+        }
+
+        let font = CGFont(provider)!
+        let error: UnsafeMutablePointer<Unmanaged<CFError>?>? = nil
+
+        guard CTFontManagerRegisterGraphicsFont(font, error) else {
+            guard
+                let unError = error?.pointee?.takeUnretainedValue(),
+                let description = CFErrorCopyDescription(unError)
+            else {
+                print("Unknown error")
+                return nil
+            }
+            print(description)
+            
+        }
+        
+        let fontName = String(font.fullName!)
+        
+        return UIFont(name: fontName, size: 20)
+    }
+    
 
 }
 
